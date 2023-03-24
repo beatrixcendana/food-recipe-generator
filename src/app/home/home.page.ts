23b80c0d1d195250968a5d7fa9b0ead0f88a4c4b
yogat@iseka.services <yogat@iseka.services>
2023-03-24 09:39:32 +0800

    initial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public ingredient_names: string = '';
+  public ingredients_arr: any = [];
+
+  private handlerMessage: string = '';
+
+  constructor(private alertCtrl: AlertController) {}
+
+  async onCommitSubmit() {
+    this.ingredients_arr = this.ingredient_names.split(',');
+    console.log(this.ingredients_arr)
+  }
+
+  async onCommitClear() {
+    const alert = await this.alertCtrl.create({
+      header: 'Info!',
+      message: 'Are you sure want to clear?',
+      buttons: [
+        {
+          text: 'Yes, Proceed',
+          role: 'confirm',
+          handler: () => {
+            this.ingredients_arr.length = 0;
+            this.handlerMessage = 'Ingredients succesfully cleared!'
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
